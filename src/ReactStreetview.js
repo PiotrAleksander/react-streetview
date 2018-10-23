ee86517f18dbec61c932bddf4d1d31c4d6f7c6ab
unknown <piotrhugonow@gmail.com>
2018-10-23 14:59:09 +0200

    radius added

--- src/ReactStreetview.js
+++ src/ReactStreetview.js
@@ -1,80 +1,111 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-import asyncLoading from 'react-async-loader';
+import asyncLoading from "react-async-loader";
 
 class ReactStreetview extends React.Component {
-
 	constructor() {
 		super();
-		this.streetView = null;
+		this.streetViewPanorama = null;
+		this.streetViewService = null;
 	}
 
-	initialize (canvas) {
-		if (this.props.googleMaps && this.streetView == null) {
-			this.streetView = new this.props.googleMaps.StreetViewPanorama(
-				canvas,
-				this.props.streetViewPanoramaOptions
+	processStreetViewData = (data, status) => {
+		const {
+			streetViewPanoramaOptions,
+			onPositionChanged,
+			onPovChanged,
+			noStreetViewFound
+		} = this.props;
+
+		if (status === "OK") {
+			this.streetViewPanorama.setPano(data.location.pano);
+			this.streetViewPanorama.setPov(streetViewPanoramaOptions.pov);
+			this.streetViewPanorama.setVisible(true);
+
+			this.streetViewPanorama.addListener("position_changed", () => {
+				if (onPositionChanged) {
+					onPositionChanged(this.streetViewPanorama.getPosition());
+				}
+			});
+
+			this.streetViewPanorama.addListener("pov_changed", () => {
+				if (onPovChanged) {
+					onPovChanged(this.streetViewPanorama.getPov());
+				}
+			});
+		} else {
+			noStreetViewFound(status);
+		}
+	};
+
+	initialize(canvas) {
+		const { googleMaps, streetViewPanoramaOptions, radius } = this.props;
+		if (googleMaps && this.streetViewPanorama === null) {
+			this.streetViewService = new this.props.googleMaps.StreetViewService();
+			this.streetViewPanorama = new googleMaps.StreetViewPanorama(canvas);
+			console.log(streetViewPanoramaOptions.position);
+			this.streetViewService.getPanorama(
+				{ location: streetViewPanoramaOptions.position, radius },
+				this.processStreetViewData
 			);
-
-			this.streetView.addListener('position_changed',() => {
-				if (this.props.onPositionChanged) {
-					this.props.onPositionChanged(this.streetView.getPosition());
-				}
-			});
-
-			this.streetView.addListener('pov_changed',() => {
-				if (this.props.onPovChanged) {
-					this.props.onPovChanged(this.streetView.getPov());
-				}
-			});
 		}
 	}
 
-	componentDidMount () {
+	componentDidMount() {
 		this.initialize(ReactDOM.findDOMNode(this));
 	}
 
-	componentDidUpdate () {
+	componentDidUpdate() {
 		this.initialize(ReactDOM.findDOMNode(this));
 	}
-	componentWillUnmount () {
-		if (this.streetView) {
-			this.props.googleMaps.event.clearInstanceListeners(this.streetView);
+	componentWillUnmount() {
+		if (this.streetViewPanorama) {
+			this.props.googleMaps.event.clearInstanceListeners(
+				this.streetViewPanorama
+			);
 		}
 	}
 
-	render () {
-		return <div
-			style = {{
-				height: '100%'
-			}}
-		></div>;
+	render() {
+		return (
+			<div
+				style={{
+					height: "100%"
+				}}
+			/>
+		);
 	}
 }
 
 ReactStreetview.propTypes = {
 	apiKey: PropTypes.string.isRequired,
-	streetViewPanoramaOptions: PropTypes.object.isRequired,
+	streetViewPanoramaOptions: PropTypes.object,
+	searchRadius: PropTypes.number,
+	noStreetViewFound: PropTypes.func,
 	onPositionChanged: PropTypes.func,
 	onPovChanged: PropTypes.func
 };
 
 ReactStreetview.defaultProps = {
-	streetViewPanoramaOptions : {
-		position: {lat: 46.9171876, lng: 17.8951832},
-		pov: {heading: 0, pitch: 0},
+	streetViewPanoramaOptions: {
+		position: { lat: 46.9171876, lng: 17.8951832 },
+		pov: { heading: 0, pitch: 0 },
 		zoom: 1
+	},
+	searchRadius: 50,
+	noStreetViewFound(status) {
+		console.log(`No street view found: ${status}`);
 	}
 };
 
-function mapScriptsToProps (props) {
+function mapScriptsToProps(props) {
 	const googleMapsApiKey = props.apiKey;
 	return {
 		googleMaps: {
-			globalPath: 'google.maps',
-			url: 'https://maps.googleapis.com/maps/api/js?key=' + googleMapsApiKey,
+			globalPath: "google.maps",
+			url: "https://maps.googleapis.com/maps/api/js?key=" + googleMapsApiKey,
 			jsonp: true
 		}
 	};
